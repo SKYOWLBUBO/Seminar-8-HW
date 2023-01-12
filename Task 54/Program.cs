0239b606d59688e2066581f9f59afe8a9ba2a288
Matvey Firsov <firsov.mat@yandex.ru>
2023-01-12 18:46:43 +0300

    HW

---
+++ Task 54/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] array = new int[3, 4];
+FillArrayRandom(array);
+PrintArray(array);
+SortToLower(array);
+Console.WriteLine();
+PrintArray(array);
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int d = 0; d < array.GetLength(1) - 1; d++)
+            {
+                if (array[i, d] < array[i, d + 1])
+                {
+                    int c = array[i, d + 1];
+                    array[i, d + 1] = array[i, d];
+                    array[i, d] = c;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Task 56/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] array ={
+    {1,4,7,2},
+    {5,9,2,3},
+    {8,4,2,4},
+    {5,2,6,7}
+};
+System.Console.WriteLine($"Номер строки с минимальной суммой элементов = {MinRow(array)}");
+
+int MinRow(int[,] array)
+{
+    int result = 0; // номер строки с мин суммой
+    int min_sum = 0;
+    for(int j = 0; j < array.GetLength(0); j++)
+    {
+        System.Console.Write(array[0,j] + " ");
+        min_sum += array[0,j];
+    }
+    System.Console.Write("\t Сумма 0-ой строки =" + min_sum);
+
+    for(int i = 1; i < array.GetLength(0); i++)
+    {
+        int temp = 0;
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i,j] + " ");
+            temp +=array[i,j];
+        }
+        System.Console.WriteLine($"\t Сумма {i} строки = {temp}");
+
+        if(temp < min_sum)
+        {
+            min_sum = temp;
+            result = i;
+        }
+    }
+    return result;
+}
